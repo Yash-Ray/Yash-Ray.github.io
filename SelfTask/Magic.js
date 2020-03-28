f0be873279a3be249f37d05ab4217397f88cc7a2
Yash <ray.yash.08@gmail.com>
2020-03-28 20:10:07 +0530

    Added Self Task

---
+++ SelfTask/Magic.js
@@ -0,0 +1,26 @@
+function find()
+{
+    var x=document.getElementById('Input').value.split(",").map(Number);
+    // console.log(x);
+    var y=document.getElementById('Input').value;
+    var len=(y.length+1)/2;
+    var max=x[0];
+    var sum=0;
+    var avg=0.0;
+    for(i=0;i<len;i++)
+    {
+        if(x[i]>max)
+        max=x[i];
+        sum+=x[i];
+    }
+    avg=sum/len;
+
+    console.log("Sum = ", sum);
+    console.log("Average = ", avg);
+    console.log("Max = ", max);
+    alert("Hello");
+
+    // document.getElementById('sum').textContent=sum;
+    // document.getElementById('avg').textContent=avg;
+    // document.getElementById('max').textContent=max;
+}
